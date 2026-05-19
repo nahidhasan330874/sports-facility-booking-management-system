6d7d44c8d137444fe157e0111c3b88095b277793
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-05-19 10:50:16 +0600

    added the navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    
-    </div>
+  <div>
+     <h1> Home page</h1>
+  </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+ "use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import Image from "next/image";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "All Facilities", path: "/facilities" },
+    { name: "My Bookings", path: "/bookings" },
+    { name: "Add Facility", path: "/add-facility" },
+    { name: "Manage My Facilities", path: "/manage-facilities" },
+  ];
+
+  return (
+    <nav className="sticky top-0 z-50  bg-white backdrop-blur-md shadow">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+        
+    
+        <Link href="/" className="flex items-center gap-1">
+          <div className="flex items-center justify-center">
+              <Image
+                src={"/image.png"}
+                alt="logo"
+                loading="eager"
+                width={40}
+                height={40}
+                className="object-cover h-auto w-auto  "
+              />
+          </div>
+
+          <div>
+            <h1 className="text-xl font-bold text-gray-800">
+              Sport<span className="text-[#00FF9D]">Nest</span>
+            </h1>
+          </div>
+        </Link>
+
+      
+        <ul className="hidden items-center gap-8 lg:flex">
+          {navLinks.map( link => (
+            <li key={link.name}>
+              <Link
+                href={link.path}
+                className="text-sm font-medium text-gray-700 transition hover:text-[#00FF9D]"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+ 
+        <div className="hidden items-center gap-3 lg:flex">
+          <Link
+            href="/signin"
+            className="rounded-xl border border-[#00FF9D] px-5 py-2 text-sm font-semibold text-[#00FF9D] transition hover:bg-[#00FF9D] hover:text-black"
+          >
+            Sign In
+          </Link>
+
+          <Link
+            href="/signup"
+            className="rounded-xl bg-[#00FF9D] px-5 py-2 text-sm font-semibold text-black transition hover:scale-105"
+          >
+            Sign Up
+          </Link>
+        </div>
+
+     
+        <button
+          onClick={() => setOpen(!open)}
+          className="text-gray-700 lg:hidden"
+        >
+          {open ? <X size={28} /> : <Menu size={28} />}
+        </button>
+      </div>
+
+  
+      {open && (
+        <div className="border-t border-gray-800 bg-[#111827] lg:hidden">
+          <ul className="flex flex-col gap-5 px-6 py-6">
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.path}
+                  onClick={() => setOpen(false)}
+                  className="block text-gray-300 transition hover:text-[#00FF9D]"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+
+            <div className="flex flex-col gap-3 pt-4">
+              <Link
+                href="/signin"
+                className="rounded-xl border border-[#00FF9D] px-5 py-2 text-center font-semibold text-[#00FF9D]"
+              >
+                Sign In
+              </Link>
+
+              <Link
+                href="/signup"
+                className="rounded-xl bg-[#00FF9D] px-5 py-2 text-center font-semibold text-black"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
