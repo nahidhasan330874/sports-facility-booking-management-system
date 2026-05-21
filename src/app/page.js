b0e559dfeea5c30a1cc68dbbcb1ec4cef5280d49
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-05-21 20:41:30 +0600

    added the 6 facilitycard with home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import HomeCard from "@/components/HomeCard";
  
 
 export default function Home() {
   return (
   <div>
       <Banner></Banner>
+      <HomeCard></HomeCard>
   </div>
   );
 }
---
+++ src/components/HomeCard.jsx
@@ -0,0 +1,111 @@
+import { Button, Card } from "@heroui/react";
+import Link from "next/link";
+import Image from "next/image";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+async function getFacilities() {
+  try {
+    const res = await fetch("http://localhost:5000/facilities?limit=6", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export default async function HomeCard() {
+  const facilities = await getFacilities();
+
+  return (
+    <section className="max-w-7xl mx-auto py-10 px-4 my-10">
+      <h1 className="text-3xl font-bold mb-8">Featured Facilities</h1>
+
+      <div
+        className="
+            grid
+            grid-cols-1
+            md:grid-cols-2
+            lg:grid-cols-3
+             gap-5
+             mb-5
+          "
+      >
+        {facilities.map((facility) => (
+          <Card
+            key={facility._id}
+            className="group bg-white border border-gray-200 shadow-md hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 rounded-3xl overflow-hidden"
+          >
+            <div className="overflow-hidden relative">
+              <Image
+                src={facility.imageUrl}
+                alt={facility.facilityName}
+                width={600}
+                height={400}
+                className="w-full h-45 object-cover group-hover:scale-110 transition-transform duration-500"
+              />
+
+              <div className="absolute top-4 right-4 bg-[#00FF9D] text-black text-sm font-bold px-4 py-1 rounded-full shadow">
+                Available
+              </div>
+            </div>
+
+            <div className="p-2 space-y-3">
+              <div>
+                <h2 className="text-2xl font-bold text-gray-900">
+                  {facility.facilityName}
+                </h2>
+
+                <p className="text-sm text-gray-500 ">
+                  Premium Sports Facility
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2 text-gray-600">
+                <FaLocationDot className="text-[#00FF9D] text-lg" />
+                <p className="text-sm font-medium">{facility.location}</p>
+              </div>
+
+              <div className="border-t my-4 border-gray-200"></div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex gap-2">
+                  <h3 className="text-xl font-extrabold text-gray-900">
+                    ${facility.price}
+                  </h3>
+                  <span className="text-sm text-gray-500">per hour</span>
+                </div>
+
+                <Link href={`/facility/${facility._id}`}>
+                  <Button
+                    radius="full"
+                    className="bg-[#00FF9D] hover:bg-[#00e68a] text-black font-semibold px-6 py-2 shadow-lg"
+                  >
+                    Book Now
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+
+ <div className="flex justify-end">
+         <Button
+        radius="full"
+        className="bg-[#005836] hover:bg-[#00431a] text-white font-bold px-6 py-2 shadow-lg"
+      >
+        
+         All Facility <FaAngleDoubleRight />
+      </Button> 
+ </div>
+    </section>
+  );
+}
